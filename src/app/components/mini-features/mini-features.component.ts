7d0010e12714eb4a316841c8dea8c0dbd6f6ef1c
Bittu <bittu1040@gmail.com>
2024-12-17 17:00:53 +0530

    tested pagination-ng npm package

--- src/app/components/mini-features/mini-features.component.ts
+++ src/app/components/mini-features/mini-features.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CheckUsernameUniqueComponent } from '../../mini-features/check-username-unique/check-username-unique.component';
 import { DateTimeDemoComponent } from '../../mini-features/date-time-demo/date-time-demo.component';
 import { PaginationUIComponent } from "../../mini-features/pagination-ui/pagination-ui.component";
+import { PaginationNgComponent, PaginationNgService } from '@bittu1040/pagination-ng';
 
 @Component({
   selector: 'app-mini-features',
   standalone: true,
-  imports: [CheckUsernameUniqueComponent, DateTimeDemoComponent, PaginationUIComponent],
+  imports: [CheckUsernameUniqueComponent, DateTimeDemoComponent, PaginationUIComponent, PaginationNgComponent],
   templateUrl: './mini-features.component.html',
   styleUrl: './mini-features.component.scss'
 })
