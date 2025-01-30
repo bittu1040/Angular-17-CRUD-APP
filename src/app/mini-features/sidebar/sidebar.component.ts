5f6e258144b61ede2e6d2c8b44ca74882e58b68f
Raunak88 <raunak.kumar88@gmail.com>
2025-01-30 15:11:20 +0100

    added sidebar to table

---
+++ src/app/mini-features/sidebar/sidebar.component.ts
@@ -0,0 +1,34 @@
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  SimpleChanges,
+  OnChanges,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss',
+})
+export class SidebarComponent implements OnChanges {
+  @Input() open: boolean = false;
+  @Input() data!: { [key: string]: any };
+  @Output() sideBarToggled: EventEmitter<boolean> = new EventEmitter();
+  columns!: string[];
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['data']?.currentValue) {
+      console.log(changes['data'].currentValue);
+      this.columns = Object.keys(changes['data'].currentValue);
+    }
+  }
+
+  toggleSidebar() {
+    this.open = !this.open;
+    this.sideBarToggled.emit(this.open);
+  }
+}
